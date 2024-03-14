992fef1854b26ca3585f38cfcd59bff31740950f
leomirco <leomirco@gmail.com>
2024-03-14 12:50:46 +0100

    debug

--- tybotRoute/test/conversation-brevo_bot.js
+++ tybotRoute/test/conversation-brevo_bot.js
@@ -22,7 +22,7 @@ const bot = {
 					"attributes": {
 					  "FIRSTNAME": "Ellya",
 					  "LASTNAME":"Ninas",
-    				  "SMS":"+393384147231"
+    				  "SMS":"+393384147632"
 					},
 					"emailBlacklisted": false,
 					"smsBlacklisted": false,
